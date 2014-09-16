76e184e40ea7f4b6594fec7002ebc31348258c5d
Tim Hopkins <th58@zips.uakron.edu>
2014-09-16 16:40:23 -0400

    Initial commit

---
+++ kernel.c
@@ -0,0 +1,96 @@
+void printString(char*);
+void readString(char*);
+int mod(int, int);
+int div(int, int);
+void writeInt(int);
+void readInt(int*);
+
+void main()
+{
+  char line[80];
+  int x;
+
+  
+  printString("Enter a line: \0");
+  readString(line);
+  printString("\r\nYou typed: \0");
+  printString(line);
+  printString("\r\n\0");
+  printString("Enter a number: \0");
+  readInt(&x);
+  printString("You Entered \0");
+  writeInt(x);
+  printString("\r\n\0");
+  while(1);
+}
+
+
+void printString(char* ch)
+{
+  /* char* ch;*/
+  int i = 0;
+  while (ch[i]!= '\0'){
+    char al = ch[i];
+    char ah = 14;
+    int ax = ah * 256 + al;
+    interrupt(16, ax, 0, 0, 0);
+    i = i + 1;
+  }
+
+ }
+
+void readString(char* ch)
+{
+  /*char* ch;*/
+  int i = 0;
+  do{
+    ch[i] = interrupt(22,0,0,0,0);
+    i = i + 1;
+  } while(ch[i -1] != 13);
+
+  ch[i - 1] = 0x0;
+}
+
+int mod(int a, int b)
+{
+  int x = a;
+  while (x >= b) x = x - b;
+  return x;
+}
+
+int div(int a, int b)
+{
+  int q = 0;
+  while(q * b <= a) q++;
+  return (q - 1);
+}
+
+void writeInt(int x)
+{
+  char number[6], *d;
+  int q = x, r;  d = number + 5;
+  *d = 0; 
+  d--;
+  while(q > 0)
+    {
+      r = mod(q,10);
+      q = div(q,10);
+      *d = r + 48; 
+      d--;
+    }
+  d++;
+  printString(d);
+}
+
+void readInt(int* x)
+{
+  char number[6];
+  int i = 0;
+  *x = 0;
+  readString(number);
+  for(; number[i] != 0; i++)
+  {
+    *x = (*x) * 10;
+    *x = number[i] - 48;
+  } 
+}
